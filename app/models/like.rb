edff7eb99e4f42260f7a4f808c628e5e4c69c001
Alan Carrie <alan.carrie@onfido.com>
2020-06-29 14:18:54 +0100

    scope for recent likes, ranking score

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,6 @@ class Like < ApplicationRecord
   belongs_to :profile
 
   validates :by, uniqueness: true
+
+  scope :recent, -> { where(score: 1).where('created_at > ?', Time.now - 2.hours) }
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,30 @@ class Profile < ApplicationRecord
 
   validates :name, uniqueness: true
 
-  def total_score
+  def net_likes
     likes.sum(:score)
+  end
+
+  def ranking_score
+    0.8 * day_likes + 0.6 * vowel_consonant_ratio
+  end
+
+  private
+
+  def vowel_consonant_ratio
+    (count_vowels / count_consonants).to_f
+  end
+
+  def count_consonants
+    all_characters = description.delete(' ').length
+    all_characters - count_vowels
+  end
+
+  def count_vowels
+    description.downcase.count('aeiou')
+  end
+
+  def day_likes
+    likes.recent.count
   end
 end
